28039f5febba578a9b7530c8bf06c9b381d24673
Bbllaaddee <mr.kamorzin@yandex.ru>
2022-05-04 17:04:04 +0300

    lesson 14: AJAXUsing LibreTranslate instead of corporate bs

---
+++ microblog/app/translate.py
@@ -0,0 +1,17 @@
+import json
+import requests
+from flask_babel import _
+from app import app
+
+def translate(text, source_language, dest_language):
+    headers = {'Content-Type': 'application/json'}
+
+    r = requests.post('https://libretranslate.de/translate', headers=headers,
+            json={'q': text, 'source': source_language, 'target': dest_language, 'format': "text"})
+
+
+    if r.status_code != 200:
+        print(r.json())
+        return _('Error: translation service failed')
+
+    return r.json()['translatedText']
