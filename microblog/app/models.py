ad65ac1639b4be4ea95eccf8fd27afc4683e6570
Bbllaaddee <mr.kamorzin@yandex.ru>
2022-05-11 19:23:07 +0300

    lesson 21: notifications and messages

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -7,6 +7,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from app import db, login
 from app.search import add_to_index, remove_from_index, query_index
+import json
+from time import time
 
 class SearchableMixin:
     @classmethod
@@ -76,7 +78,7 @@ class User(UserMixin, db.Model):
     messages_received = db.relationship('Message',
             foreign_keys='Message.recipient_id', backref='recipient', lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
-    # notifications = db.relationship('Notification')
+    notifications = db.relationship('Notification', backref='user', lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -112,6 +114,12 @@ class User(UserMixin, db.Model):
     def new_messages(self):
         last_read_time = self.last_message_read_time or datetime(1900,1,1)
         return Message.query.filter_by(recipient=self).filter(Message.timestamp > last_read_time).count()
+
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
 
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
@@ -151,3 +159,15 @@ class Message(db.Model):
 
     def __repr__(self):
         return 'Message {}'.format(self.body)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
+
